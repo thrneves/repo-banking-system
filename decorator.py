e523c2da070927ab731389168447df79f7d28c3c
thrneves <thalita.araujodosreis@gmail.com>
2026-06-17 08:39:17 -0300

    Transferring log records to a .txt file.

--- decorator.py
+++ decorator.py
@@ -1,12 +1,33 @@
 import functools
 from datetime import datetime
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent
+
 
 def log(function):
     @functools.wraps(function)
     def pack_log(*args, **kwargs):
+        arguments = None
         date = datetime.now()
-        print(f"Running {function.__name__} on {date}")
         response = function(*args, **kwargs)
+
+        if args and kwargs:
+            arguments = args + kwargs
+        elif args:
+            arguments = args
+        elif kwargs:
+            arguments = kwargs
+        else:
+            arguments = "No arguments"
+
+        log = f"Running: {function.__name__} on {date} - Arguments: {arguments} - Type: {type(response)}. Return: {response}\n"
+        try:
+            with open(ROOT_PATH / "log.txt", "a", encoding="utf-8") as data:
+                data.write(log)
+        except IOError as err:
+            print(f"Erro ao manipular o arquivo: {err}")
+
         return response
 
     return pack_log
